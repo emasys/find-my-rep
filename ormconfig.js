87c9a350486a135f5129e12cda29fa74559b9eb9
Emmanuel Ndukwe <emasysnd@gmail.com>
2019-11-14 13:16:23 +0100

    integrate travis ci

--- ormconfig.js
+++ ormconfig.js
@@ -3,20 +3,25 @@ require('dotenv/config');
 const database = {
   development: "find-my-rep",
   production: null,
-  test: 'test-fmr'
+  test: process.env.TEST_DB || 'test-fmr',
 }
 
+const entities = {
+  development: 'dist/**/*.entity{.ts,.js}',
+  production: null,
+  test: __dirname + '/**/*.entity.{ts,js}',
+}
 
 module.exports = {
   type: 'postgres',
   host: 'localhost',
-  port: 5432,
-  username: 'emasys',
-  password: 'root',
+  port: process.env.PORT || 5432,
+  username: process.env.USERNAME || 'emasys',
+  password: process.env.PASSWORD || 'root',
   seeds: ['seeds/*.ts'],
   factories: ['factories/**/*.factory.ts'],
   database: database[process.env.NODE_ENV],
-  entities: ['dist/**/*.entity{.ts,.js}'],
+  entities: [entities[process.env.NODE_ENV]],
   synchronize: true,
   migrationsTableName: 'migration',
   migrations: ['migration/*.js'],
